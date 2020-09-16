0ffdd031d4b714bb6f7a5e851f2fb14cbb2ba3e8
Khalil <khalilfoulks@gmail.com>
2020-09-16 17:26:27 -0400

    finished moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,21 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# given an unsorted array of integers
+# move all non 0s to the left of 0
+# return the updated array
+
+# what if no 0's?; return unaltered array
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    # loop through each num in array
+    # if num is not 0 put in 1st array 
+    # if num is 0 put in 2nd array 
+    # combine the arrays
+    newArr = [num for num in arr if num != 0] + [num for num in arr if num == 0]
+    return newArr
 
 
 if __name__ == '__main__':
